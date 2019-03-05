6f47f001d3ef419458d28b56875404b77f95e208
taiyong <taiyong@youzan.com>
2019-03-05 20:24:36 +0800

    init

--- src/server/socks5/auth/None.js
+++
@@ -1,9 +0,0 @@
-module.exports = function NoneAuthHandlers() {
-    return {
-        needUserName: false,
-        METHOD: 0x00,
-        server: function serverHandler(stream, cb) {
-            cb(true, '', '');
-        }
-    };
-};
